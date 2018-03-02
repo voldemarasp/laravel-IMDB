2763c32c3d1b765fd571525774e78897e5e07cbc
voldemarasp <voldemarasp@gmail.com>
2018-03-02 14:26:18 +0200

    last before deploy

--- database/seeds/ImagesSeeder.php
+++ database/seeds/ImagesSeeder.php
@@ -22,14 +22,20 @@ class ImagesSeeder extends Seeder
         $images = 'https://api.themoviedb.org/3/movie/'. $movie->id . '/images?api_key=3d666046197bc35f402080e836eaaa66';
 		$jsonImages = json_decode(file_get_contents($images), true); 
 
+
+        $count = 0;
+
 		    foreach ($jsonImages['backdrops'] as $seedas) {
+            $count++;
             $getFullUrl = 'http://image.tmdb.org/t/p/w300/' . $seedas['file_path'];
             $downloadImage = file_get_contents($getFullUrl);
             $filename = md5($seedas['file_path']);
             $ext = pathinfo($getFullUrl, PATHINFO_EXTENSION);
             $fullFileName = $filename .".". $ext;
 
-            $if ($jsonImages['backdrops']->first() == $seedas) {
+
+
+            $if ($count == 1) {
                 $featured_image = 'yes';
             } else {
                 $featured_image = '';
